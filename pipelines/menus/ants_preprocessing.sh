99f838349e6db46d724276c6c5067c209080b251
N.M. van Strien <niels@rad01.homelinux.org>
2012-01-05 18:02:44 +0100

    ENH: added preprocessing (yet still under construction)

---
+++ pipelines/menus/ants_preprocessing.sh
@@ -0,0 +1,324 @@
+#!/bin/bash
+
+preprocessing_config_01 () {  # run motion correction yes/no?
+clear
+mc_setup_chk=`cat ${exp_path}/scripts/dependencies.sh | grep mc`
+
+if [ ${#mc_setup_chk} == 0 ]
+then
+# add variable to dependencies.sh to indicate if motion correction should be run
+echo "" >> ${exp_path}/scripts/dependencies.sh 
+echo "# Run motion correction (0=no; 1=yes)" >> ${exp_path}/scripts/dependencies.sh 
+echo "mc=0" >> ${exp_path}/scripts/dependencies.sh 
+
+# add variable to dependencies.sh to indicate if motion correction was completed
+echo "" >> ${exp_path}/scripts/dependencies.sh 
+echo "# Motion correction complete (0=no; 1=yes)" >> ${exp_path}/scripts/dependencies.sh 
+echo "mc_pass=0" >> ${exp_path}/scripts/dependencies.sh 
+current=`cat ${exp_path}/scripts/dependencies.sh | grep mc`
+else
+current=`cat ${exp_path}/scripts/dependencies.sh | grep mc`
+fi
+
+while : # Loop forever
+do
+cat <<MENU
+Run Motion correction?
+
+1) No
+2) Yes
+
+MENU
+
+echo -n " Your choice? : "
+read choice
+
+case $choice in
+1) sed s/"${current}"/"mc=0"/g ${exp_path}/scripts/dependencies.sh > ${exp_path}/scripts/dependencies_tmp.sh; 
+   mv ${exp_path}/scripts/dependencies_tmp.sh ${exp_path}/scripts/dependencies.sh; 
+   preprocessing_config_02;;
+2) sed s/"${current}"/"mc=1"/g ${exp_path}/scripts/dependencies.sh > ${exp_path}/scripts/dependencies_tmp.sh; 
+   mv ${exp_path}/scripts/dependencies_tmp.sh ${exp_path}/scripts/dependencies.sh; 
+   preprocessing_config_02;;
+*) echo "\"$choice\" is not valid "; sleep 2 ;;
+esac
+done
+
+}
+
+preprocessing_config_02 () { # run brain extraction yes/no?
+clear
+bxt_setup_chk=`cat ${exp_path}/scripts/dependencies.sh | grep bxt`
+
+if [ ${#bxt_setup_chk} == 0 ]
+then
+echo "" >> ${exp_path}/scripts/dependencies.sh 
+echo "# Run Brain Extraction (0=no; 1=yes)" >> ${exp_path}/scripts/dependencies.sh 
+echo "bxt=0" >> ${exp_path}/scripts/dependencies.sh 
+
+# add variable to dependencies.sh to indicate if brain extraction was completed
+echo "" >> ${exp_path}/scripts/dependencies.sh 
+echo "# Motion correction complete (0=no; 1=yes)" >> ${exp_path}/scripts/dependencies.sh 
+echo "bxt_pass=0" >> ${exp_path}/scripts/dependencies.sh 
+
+current=`cat ${exp_path}/scripts/dependencies.sh | grep bxt`
+else
+current=`cat ${exp_path}/scripts/dependencies.sh | grep bxt`
+fi
+
+while : # Loop forever
+do
+cat <<MENU
+Run Brain Extraction? Currently not recommended for partial field of view data.
+
+1) No
+2) Yes
+
+MENU
+
+echo -n " Your choice? : "
+read choice
+
+case $choice in
+1) sed s/"${current}"/"bxt=0"/g ${exp_path}/scripts/dependencies.sh > ${exp_path}/scripts/dependencies_tmp.sh; 
+   mv ${exp_path}/scripts/dependencies_tmp.sh ${exp_path}/scripts/dependencies.sh; 
+   review_start;;
+2) sed s/"${current}"/"bxt=1"/g ${exp_path}/scripts/dependencies.sh > ${exp_path}/scripts/dependencies_tmp.sh; 
+   mv ${exp_path}/scripts/dependencies_tmp.sh ${exp_path}/scripts/dependencies.sh; 
+   review_start;;
+*) echo "\"$choice\" is not valid "; sleep 2 ;;
+esac
+done
+}
+
+review_start () { # review preprocessing choices and start batch
+clear
+source ${exp_path}/scripts/dependencies.sh
+while : # Loop forever
+do
+cat <<MENU
+-------------------------------------------------------------------------------
+ANTS functional MRI data processing pipeline - Review preprocessing & run
+-------------------------------------------------------------------------------
+
+Review your choices and create the datastructure
+
+experiment root: 	${exp_path}
+data root:		${exp_path}/data
+results:		${resultshome}
+
+no sessions:		${ses}
+no runs:		${runs}
+
+run motion correction:	${mc}
+run brain extraction:	${bxt}
+
+
+
+Start pre-processing (y/n)?
+
+
+-------------------------------------------------------------------------------
+Written by Brian Avants & Niels van Strien, 2011/2012
+-------------------------------------------------------------------------------
+MENU
+
+echo -n " Your choice? : "
+read choice
+
+case $choice in
+y) run_preprocessing ;;
+n) preprocessing_menu ;;
+*) echo "\"$choice\" is not valid "; sleep 2 ;;
+esac
+
+done
+}
+
+run_preprocessing () { # main control structure for looping through data and running preprocessing
+    clear
+    source ants_functionlib # a textfile with ants-functions that are used in different scripts
+    source ${exp_path}/scripts/dependencies.sh # the user configuration file
+    mkdir -p ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation # make a dir for template creation
+    for ((i = 1; i <= ${ses} ; i++)) # looping through sessions
+    do
+      if [ ${i} -lt 10 ] # dealing with leading zero in session dirname 
+      then
+	for ((j = 1; j <= ${runs} ; j++)) # looping through runs
+	do
+	if [ ${j} -lt 10 ] # dealing with leading zero in run dirname
+	then
+	subarray=(`find ${exp_path}/data/session_0${i}/run_0${j}/* -prune -type f | tr " " "\n"`) # a real bash array with subject names
+
+	# create average images of time series; needed for SSBT if no other processing options are used
+	if [ ! -d ${exp_path}/data/session_0${i}/run_0${j}/avg ]; then mkdir ${exp_path}/data/session_0${i}/run_0${j}/avg; fi
+
+	for ((k = 0; k <= ${#subarray[@]}-1 ; k++))
+	do
+	outfname=$(basename $(echo ${subarray[k]} | cut -d '.' -f 1 )) # nested command execution; neat!
+	OUT="${exp_path}/data/session_0${i}/run_0${j}/avg/${outfname}"
+
+	if [ ! -f ${OUT}_avg.nii.gz ]
+	then
+	ants_moco -d 3 -a ${subarray[k]} -o ${OUT}_avg.nii.gz 
+	echo "${exp_path}/data/session_0${i}/run_0${j}/avg/${outfname}_avg.nii.gz" >> ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects.txt
+	fi
+	done
+
+	  if [ ${mc} -eq 1 ] # run motion correction if mc=1
+	  then
+	      
+	      if [ -f ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects.txt ]
+	      then
+	      rm ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects.txt # will be replaced by motion corrected fnames
+	      fi
+
+	      mkdir -p ${resultshome}/preprocessing/motion_correction/data/session_0${i}/run_0${j}/
+	      for ((k = 0; k <= ${#subarray[@]}-1 ; k++))
+	      do
+		outfname=$(basename $(echo ${subarray[k]} | cut -d '.' -f 1 )) # nested command execution; neat!
+		OUT="${resultshome}/preprocessing/motion_correction/data/session_0${i}/run_0${j}/${outfname}"
+		infname=${subarray[k]}
+		Procedure="Motion correction session_0${i} run_0${j} subject: ${outfname}"
+		showBar ${k} ${#subarray[@]} ${Procedure} # show progress bar
+#		mc_ants_rigid ${infname} ${OUT} # run motion correction
+		rm ${resultshome}/preprocessing/motion_correction/data/session_0${i}/run_0${j}/${outfname}*.txt # remove progress file
+		echo "${resultshome}/preprocessing/motion_correction/data/session_0${i}/run_0${j}/${outfname}_avg.nii.gz" >> ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects_mc.txt
+		sleep 0.5 # debugging only
+	      done
+
+	      # change variable in dependencies.sh to indicate that motion correction was completed
+	      sed s/"mc_pass=0"/"mc_pass=1"/g ${exp_path}/scripts/dependencies.sh > ${exp_path}/scripts/dependencies_tmp.sh; 
+	      mv ${exp_path}/scripts/dependencies_tmp.sh ${exp_path}/scripts/dependencies.sh; 
+	      source ${exp_path}/scripts/dependencies.sh; 
+	      clear
+
+	  fi
+
+	  if [ ${bxt} -eq 1 ] # run brain extraction if bxt=1; currently only makes a mask image, but needs to make 4D masked files as well.
+	  then
+
+	      mkdir -p ${resultshome}/preprocessing/brain_extraction/data/session_0${i}/run_0${j}/
+
+	      if [ -f ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects.txt ] || [ -f ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects_mc.txt ] 
+	      then
+	      rm ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects*.txt # will be replaced by brain extracted corrected fnames
+	      fi
+
+	      for ((k = 0; k <= ${#subarray[@]}-1 ; k++))
+	      do
+		outfname=$(basename $(echo ${subarray[k]} | cut -d '.' -f 1 )) # nested command execution; neat!
+		if [ ${mc} -eq 1 ] && [ ${mc_pass} -eq 1 ] # use motion corrected results if motion correction was run
+		then 
+		  
+		  infname="${resultshome}/preprocessing/motion_correction/data/session_0${i}/run_0${j}/${outfname}_avg.nii.gz"
+		  OUT="${resultshome}/preprocessing/brain_extraction/data/session_0${i}/run_0${j}/${outfname}"
+		  ants_bxt ${infname} ${OUT}
+
+		  echo "${resultshome}/preprocessing/brain_extraction/data/session_0${i}/run_0${j}/${outfname}_avg.nii.gz" >> ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects_bxt.txt
+
+		else # use original data 
+		
+		  OUT="${resultshome}/preprocessing/brain_extraction/data/session_0${i}/run_0${j}/${outfname}"		
+		  ants_moco -d 3 -a ${subarray[k]} -o ${OUT}_avg.nii.gz 
+		  infname="${resultshome}/preprocessing/brain_extraction/data/session_0${i}/run_0${j}/${outfname}_avg.nii.gz"
+		  ants_bxt ${infname} ${OUT}
+
+		  echo "${resultshome}/preprocessing/brain_extraction/data/session_0${i}/run_0${j}/${outfname}_avg.nii.gz" >> ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects_bxt.txt
+
+		fi	      
+	      
+	      Procedure="Brain Extraction session_0${i} run_0${j} subject: ${outfname}"
+	      showBar ${k} ${#subarray[@]} ${Procedure}
+
+	    done
+
+	  # change variable in dependencies.sh to indicate that motion correction was completed
+	  sed s/"bxt_pass=0"/"bxt_pass=1"/g ${exp_path}/scripts/dependencies.sh > ${exp_path}/scripts/dependencies_tmp.sh; 
+	  mv ${exp_path}/scripts/dependencies_tmp.sh ${exp_path}/scripts/dependencies.sh; 
+	  source ${exp_path}/scripts/dependencies.sh; 
+
+	  clear
+	  fi
+
+	else # condition where leading zero in run dirname is removed (more than 10 runs!)
+	echo "mkdir -p ${exp_path}/data/session_0${i}/run_${j}"
+	fi
+	done
+      else 
+	for ((j = 1; j <= ${runs} ; j++))
+	do
+	if [ ${j} -lt 10 ]
+	then
+	echo "mkdir -p ${exp_path}/data/session_${i}/run_0${j}"
+	else
+	echo "mkdir -p ${exp_path}/data/session_${i}/run_${j}"
+	fi
+	done
+      fi
+    done
+
+# Using buildtemplateparallel.sh to create template. Need to break out of control loop to estimate template.
+btplist=`ls ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/subjects*.txt`
+if [ ! -f ${btplist} ] # this will 
+then
+echo "missing subjects list"
+else
+echo "run btp"
+exe="buildtemplateparallel.sh -d 3 -o ${resultshome}/preprocessing/SSBT_corrected/SSBT_creation/mean `cat ${btplist}` "
+echo $exe
+exit
+fi
+
+}
+
+
+
+preprocessing_menu () {
+clear
+
+while : # Loop forever
+do
+cat <<MENU
+-------------------------------------------------------------------------------
+ANTS functional MRI data processing pipeline - Preprocessing
+-------------------------------------------------------------------------------
+Preprocessing involves the following steps:
+
+- Motion correction.
+- Brain extraction.
+- BOLD study template estimation 
+- Transformation of time-series to study space
+- Band pass temporal filtering (high pass / low pass).
+- Spatial smoothing.
+- Compcorr (removal of physiological noise)
+
+These processing steps are run for the entire group.
+
+Options 
+1) Configure & run preprocessing on group
+2) Return to main menu.
+3) Exit.
+
+-------------------------------------------------------------------------------
+Written by Brian Avants & Niels van Strien, 2011/2012
+-------------------------------------------------------------------------------
+MENU
+
+echo -n " Your choice? : "
+read choice
+
+case $choice in
+1) preprocessing_config_01 ;;
+2) main_menu ;;
+3) bye ;;
+*) echo "\"$choice\" is not valid "; sleep 2 ;;
+esac
+done
+}
+
+# main script starts here
+clear
+preprocessing_menu
+
+
--- pipelines/menus/antsmain.sh
+++ pipelines/menus/antsmain.sh
@@ -14,6 +14,51 @@ exit
 
 }
 
+main_menu_nodep () {
+clear
+
+while : # Loop forever
+do
+cat <<MENU
+-------------------------------------------------------------------------------
+ANTS functional MRI data processing pipeline
+-------------------------------------------------------------------------------
+
+1) Configure or test data setup.
+
+
+2) ANTSR demo suite.
+3) Change defaults
+4) Exit ANTSR.
+
+NB: run `basename ${0}` /pathto/dependencies.sh (resulting file of 1) to start
+(pre)processing data
+
+
+The ANTSR functional MRI (fMRI) data processing pipeline allows you to perform
+common fMRI processing steps, such as motion correction, brain extraction, 
+spatial and temporal filtering. 
+
+Using the R statistical framework, advanced analyses can be carried out, such
+as resting state fMRI analysis or task fMRI analysis.
+-------------------------------------------------------------------------------
+Written by Brian Avants & Niels van Strien, 2011/2012
+-------------------------------------------------------------------------------
+MENU
+
+echo -n " Your choice? : "
+read choice
+
+case $choice in
+1) source organize_data.sh ;;
+2)  ;;
+3)  ;;
+4)  bye;;
+*) echo "\"$choice\" is not valid "; sleep 2 ;;
+esac
+done
+}
+
 main_menu () {
 clear
 
@@ -24,16 +69,16 @@ cat <<MENU
 ANTS functional MRI data processing pipeline
 -------------------------------------------------------------------------------
 
-1) Test ANTSR software requirements
-2) Configure or test data setup.
-3) FMRI preprocessing.
-4) FMRI statistics.
-5) ANTSR demo suite.
-6) Exit ANTSR.
+1) Configure or test data setup.
+2) FMRI preprocessing.
+3) FMRI statistics. (under construction)
+4) ANTsR demo suite.
+5) Change defaults
+6) Exit ANTsR.
 
 
 
-The ANTSR functional MRI (fMRI) data processing pipeline allows you to perform
+The ANTsR functional MRI (fMRI) data processing pipeline allows you to perform
 common fMRI processing steps, such as motion correction, brain extraction, 
 spatial and temporal filtering. 
 
@@ -50,8 +95,8 @@ echo -n " Your choice? : "
 read choice
 
 case $choice in
-1)  ;;
-2) source organize_data.sh ;;
+1) source organize_data.sh ;;
+2) source ants_preprocessing.sh ;;
 3)  ;;
 4)  ;;
 5)  ;;
@@ -61,10 +106,11 @@ esac
 done
 }
 
+
 cfg_file=$1
 # main script starts here
 if [ $# -lt 1 ] ; then 
-main_menu
+main_menu_nodep
 elif [ -f ${cfg_file} ] && [[ ${cfg_file} =~ "dependencies" ]]
 then
 source $1
